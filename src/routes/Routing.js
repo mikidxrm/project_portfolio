65638beebb80e0619be1a3631c698a79deeead13
Thanawat <Thanawat.k@g-able.com>
2022-01-26 16:12:07 +0700

    first commit

--- src/routes/Routing.js
+++ src/routes/Routing.js
@@ -4,7 +4,7 @@ import { HashRouter as Router, Routes,Route, Switch ,Link } from 'react-router-d
 import Dashboard from '../components/Dashboard'
 import LogIn from '../components/LogIn'
 import SignUp from '../components/SignUp'
-// import { AuthProvider } from '../components/Auth'
+import { AuthProvider } from '../components/Auth'
 import Home from '../Pages/Home';
 import Portfolio from '../Pages/portfolio';
 import About from '../Pages/about';
@@ -20,17 +20,17 @@ function Routing() {
   return (
    
       <Router>
-         
+         <AuthProvider>
         <Switch>
-          <Route exact path="/login" component={LogIn} />
+          <Route exact path="/" component={LogIn} />
           <Route exact path="/dashboard" component={Dashboard} />
           <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/" component={Home} />
+          <Route exact path="/Home" component={Home} />
           <Route exact path="/Portfolio" component={Portfolio} />
           <Route exact path="/About" component={About} />
           <Route exact path="/performmance" component={performmance} />
         </Switch>
-       
+        </AuthProvider>
       </Router>
    
   );
